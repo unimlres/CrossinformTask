e99e6c85100ed5b1ec266d81d457ac7bb9155f00
Pavel Morozov <32172934+pavelvladimirmorozov@users.noreply.github.com>
2020-03-01 21:45:00 +0300

    Консольный интерфейс

--- CrossinformTask/Program.cs
+++ CrossinformTask/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CrossinformTask.Core;
 
 namespace CrossinformTask
 {
@@ -6,7 +12,35 @@ namespace CrossinformTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите путь к файлу: ");
+
+            var path = Console.ReadLine();
+            var triplets = new Dictionary<string, int>();
+            using var source = new CancellationTokenSource();
+            var token = source.Token;
+            var timer = new Stopwatch();
+
+            timer.Start();
+            var task = TripletsFinder.FindInFileAsync(path, triplets, token);
+            task.ContinueWith(_ => timer.Stop());
+
+            Console.WriteLine("Чтобы отобразить результат нажмите на любую клавишу.\n" +
+                              "Запрос результата раньше времени приведет к остановке " + 
+                              "поиска и выводу текущих результатов");
+            Console.Write("Нажмите любую клавишу: ");
+
+            Console.ReadKey();
+
+            source.Cancel();
+            Task.Delay(500).Wait();
+
+            Console.WriteLine();
+            foreach (var (key, value) in triplets.OrderBy(o => -o.Value).Take(10))
+                Console.WriteLine($"{key} : {value}");
+
+            Console.WriteLine($"Время выполнения: {timer.ElapsedMilliseconds} мс");
+            Console.WriteLine($"\nСтатус задачи: {task.Status}");
+            Console.ReadKey();
         }
     }
 }
