98a0e6bb53e171a4df5691e738d7278a4713d1ca
Pavel Morozov <32172934+pavelvladimirmorozov@users.noreply.github.com>
2020-03-01 21:05:39 +0300

    Тест поиска

--- CrossunformTask.Tests/TripletsFinderTests.cs
+++ CrossunformTask.Tests/TripletsFinderTests.cs
@@ -1,14 +1,33 @@
-using System;
+using System.Collections.Generic;
+using CrossinformTask.Core;
 using Xunit;
 
 namespace CrossunformTask.Tests
 {
     public class TripletsFinderTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private string text = "abracadabra";
 
+        private Dictionary<string, int> result = new Dictionary<string, int>()
+        {
+            ["abr"] = 2,
+            ["bra"] = 2,
+            ["rac"] = 1,
+            ["aca"] = 1,
+            ["cad"] = 1,
+            ["ada"] = 1,
+            ["dab"] = 1
+        };
+
+        [Fact]
+        public void TestFindInString()
+        {
+            var res = new Dictionary<string, int>();
+
+            TripletsFinder.FindInString(text, res);
+
+            Assert.False(res.Count != result.Count);
+            Assert.Equal(result, res);
         }
     }
 }
